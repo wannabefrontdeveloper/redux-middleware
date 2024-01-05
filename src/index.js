e4de041b0ba0324604935d271cfbcabb703dd586
wannabefrontdeveloper <giwonk@naver.com>
2024-01-05 14:51:01 +0900

    redux-logger를 사용하도록 하였습니다.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from "./App";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
-import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
