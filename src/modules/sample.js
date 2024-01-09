6ee2c8edc560dd6208bfd627de0cc6e47c7de6de
wannabefrontdeveloper <giwonk@naver.com>
2024-01-09 15:59:34 +0900

    오타 수정

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -7,7 +7,7 @@ import createRequestThunk from "../lib/createRequestThunk";
 
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
-const GET_POST_FAILURE = "smaple/GET_POST_FAILURE";
+const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
