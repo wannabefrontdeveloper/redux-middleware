c01cb5b2d6e6d6866e16ac80baa718c10128c024
wannabefrontdeveloper <giwonk@naver.com>
2024-01-10 14:50:31 +0900

    코드수정 스토어에 redux-saga 미들웨어를 적용하였습니다.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { applyMiddleware, createStore } from "redux";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 import { Provider } from "react-redux";
 import { createLogger } from "redux-logger";
 import { thunk } from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, thunk, sagaMiddleware)
+);
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
