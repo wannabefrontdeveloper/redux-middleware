a6121f67d1136d6641f146abc5dbc3bd1626ee72
wannabefrontdeveloper <giwonk@naver.com>
2024-01-10 15:00:17 +0900

    코드 수정 리덕스 개발자 도구를 적용했습니다.

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ import { Provider } from "react-redux";
 import { createLogger } from "redux-logger";
 import { thunk } from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, thunk, sagaMiddleware)
+  composeWithDevTools(applyMiddleware(logger, thunk, sagaMiddleware))
 );
 sagaMiddleware.run(rootSaga);
 
