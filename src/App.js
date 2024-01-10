d6c20a433473769d8fe9b1a83e08bbe960bb118e
wannabefrontdeveloper <giwonk@naver.com>
2024-01-10 15:51:09 +0900

    CounterContainer를 렌더링하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import SampleContainer from "./containers/SampleContainer";
+import CounterContainer from "./containers/CounterContainer";
 const App = () => {
   return (
     <div>
-      <SampleContainer />
+      <CounterContainer />
     </div>
   );
 };
